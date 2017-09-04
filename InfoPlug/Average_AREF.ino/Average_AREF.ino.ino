977799710bad6b2c77b653f5b0dbe64270d4e103
fraggrenadekitten <camran.hansen@ucc.on.ca>
2017-09-04 17:57:16 -0400

    added Auto Aref

--- InfoPlug/Average_AREF.ino/Average_AREF.ino.ino
+++ InfoPlug/Average_AREF.ino/Average_AREF.ino.ino
@@ -4,31 +4,31 @@ float analogMin = 1024;
 
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
   byte serin = 0;
   // put your main code here, to run repeatedly:
 
-for(int i = 0;i<300;i++){
-analog[i] = analogRead(0);
-}
-
-for(int q = 0;q<300;q++){
-  if(analog[q] > analogMax){
-    analogMax = analog[q];
+  for (int i = 0; i < 300; i++) {
+    analog[i] = analogRead(0);
   }
-}
 
-for(int w = 0;w<300;w++){
-  if(analog[w] < analogMin){
-    analogMin = analog[w];
+  for (int q = 0; q < 300; q++) {
+    if (analog[q] > analogMax) {
+      analogMax = analog[q];
+    }
   }
-}
-analogMax = 0;
-analogMin = 1024;
 
-AREF = (analogMax + analogMin) / 2
+  for (int w = 0; w < 300; w++) {
+    if (analog[w] < analogMin) {
+      analogMin = analog[w];
+    }
+  }
+  analogMax = 0;
+  analogMin = 1024;
+
+  AREF = (analogMax + analogMin) / 2
 
 }
--- InfoPlug/InfoPlug3/InfoPlug3.ino
+++ InfoPlug/InfoPlug3/InfoPlug3.ino
@@ -6,75 +6,63 @@ float dataIn[149];
 float voltIn[149];
 float currentNorm = 0.00578;
 float voltNorm = 0.479;
-float watts=0;
+float watts = 0;
 float totalWatts;
-
-
+float maxA = 0;
+float minA = 1024;
+float arAref = 0;
 void setup() {
   // put your setup code here, to run once:
-Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-for(int q = 0;q<20;q++){
-  for(int i = 0; i<149;i++){
-dataIn[i] = analogRead(0);
-voltIn[i] = analogRead(1);
+  for (int q = 0; q < 20; q++) {
+    for (int i = 0; i < 149; i++) {
+      dataIn[i] = analogRead(0);
+      voltIn[i] = analogRead(1);
+      //volt
+      totalVolt += voltIn[i];
+      VREF = totalVolt / 149;
+      totalVolt = 0;
+      voltIn[i] -= VREF;
+      voltIn[i] = voltIn[i] * voltNorm;
+      //amp
+      if (dataIn[i] > maxA) {
+        maxA = dataIn[i];
+      } else if (dataIn[i] < minA) {
+        minA = dataIn[i];
+      }
+      dataIn[i] -= AREF;
+      dataIn[i] = dataIn[i] * currentNorm;
 
+    }
 
+    for (int n = 0; n < 149; n++) {
+      watts = watts + (dataIn[n] * voltIn[n]);
+    }
 
-//volt
-totalVolt += voltIn[i];
-VREF = totalVolt / 149;
-totalVolt = 0;
-voltIn[i] -= VREF;
-voltIn[i] = voltIn[i] * voltNorm;
+    arAref = arAref + (maxA + minA) / 2;
 
-
-
-//amp
-
-dataIn[i] -= AREF;
-dataIn[i] = dataIn[i] * currentNorm;
-
+    maxA = 0;
+    minA = 1024;
+    // resets variables
+    watts = watts / 149;
+    totalWatts += watts;
   }
-
-
-  
-for(int q = 0;q<149;q++){
-  if(dataIn[q] > analogMax){
-    analogMax = analog[q];
-  }
-}
-
-for(int w = 0;w<149;w++){
-  if(analog[w] < analogMin){
-    analogMin = analog[w];
-  }
-}
-analogMax = 0;
-analogMin = 1024;
-
-AREF = (analogMax + analogMin) / 2
-
-for(int n = 0;n<149;n++){
-  watts = watts + (dataIn[n] * voltIn[n]);
-}
-
-watts = watts / 149;
-totalWatts += watts;
-}
-
+  AREF = arAref / 20;
+  arAref = 0;
+  maxA = 0;
+  minA = 1024;
   totalWatts = totalWatts / 20;
-  if(totalWatts > 1){
-  Serial.println(totalWatts);
-  }else{
+  if (totalWatts > 1) {
+    Serial.println(totalWatts);
+  } else {
     Serial.println("0");
   }
   totalWatts = 0;
-
 
 
 }
