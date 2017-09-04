f3ddf19e38e4ed7b745db22a25b13640d3b0523b
Pamplemousse00 <31556084+Pamplemousse00@users.noreply.github.com>
2017-09-04 17:33:04 -0400

    yep

---
+++ InfoPlug/Average_AREF.ino/Average_AREF.ino.ino
@@ -0,0 +1,34 @@
+float analogMax;
+float analog[300];
+float analogMin = 1024;
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  byte serin = 0;
+  // put your main code here, to run repeatedly:
+
+for(int i = 0;i<300;i++){
+analog[i] = analogRead(0);
+}
+
+for(int q = 0;q<300;q++){
+  if(analog[q] > analogMax){
+    analogMax = analog[q];
+  }
+}
+
+for(int w = 0;w<300;w++){
+  if(analog[w] < analogMin){
+    analogMin = analog[w];
+  }
+}
+analogMax = 0;
+analogMin = 1024;
+
+AREF = (analogMax + analogMin) / 2
+
+}
--- InfoPlug/InfoPlug3/InfoPlug3.ino
+++ InfoPlug/InfoPlug3/InfoPlug3.ino
@@ -1,7 +1,11 @@
 int N_samples = 160;
-int VREF = 517;
-float dataIn[100];
+int AREF = 512;
+int VREF = 0;
+float totalVolt;
+float dataIn[149];
+float voltIn[149];
 float currentNorm = 0.00578;
+float voltNorm = 0.479;
 float watts=0;
 float totalWatts;
 
@@ -15,29 +19,62 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 for(int q = 0;q<20;q++){
-  for(int i = 0; i<100;i++){
+  for(int i = 0; i<149;i++){
 dataIn[i] = analogRead(0);
-dataIn[i] -= VREF;
+voltIn[i] = analogRead(1);
+
+
+
+//volt
+totalVolt += voltIn[i];
+VREF = totalVolt / 149;
+totalVolt = 0;
+voltIn[i] -= VREF;
+voltIn[i] = voltIn[i] * voltNorm;
+
+
+
+//amp
+
+dataIn[i] -= AREF;
 dataIn[i] = dataIn[i] * currentNorm;
-dataIn[i] = dataIn[i] * 120;
+
   }
 
-for(int n = 0;n<100;n++){
-  watts = watts + abs(dataIn[n]);
+
+  
+for(int q = 0;q<149;q++){
+  if(dataIn[q] > analogMax){
+    analogMax = analog[q];
+  }
 }
 
-watts = watts / 100;
+for(int w = 0;w<149;w++){
+  if(analog[w] < analogMin){
+    analogMin = analog[w];
+  }
+}
+analogMax = 0;
+analogMin = 1024;
+
+AREF = (analogMax + analogMin) / 2
+
+for(int n = 0;n<149;n++){
+  watts = watts + (dataIn[n] * voltIn[n]);
+}
+
+watts = watts / 149;
 totalWatts += watts;
 }
 
   totalWatts = totalWatts / 20;
   if(totalWatts > 1){
-  Serial.println(analogRead(0));
+  Serial.println(totalWatts);
   }else{
     Serial.println("0");
   }
   totalWatts = 0;
 
-delay(100);
+
 
 }
